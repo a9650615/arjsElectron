c794b097b795d95412966cf3d926810552986631
Ming You <a9650615@gmail.com>
2019-10-18 16:21:12 +0800

    Add select camera options

--- renderer.js
+++ renderer.js
@@ -27,6 +27,84 @@ modelLoader.load('https://a9650615.github.io/arjsElectron/data/scene.glb',
     console.error( 'An error happened', error );
   }
 )
+
+var videoSelect = document.querySelector("select#videoSource");
+var selectors = [videoSelect];
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  var values = selectors.map(function(select) {
+    return select.value;
+  });
+  selectors.forEach(function(select) {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement("option");
+    option.value = deviceInfo.deviceId;
+
+    if (deviceInfo.kind === "videoinput") {
+      option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log("Some other kind of source/device: ", deviceInfo);
+    }
+
+    selectors.forEach(function(select, selectorIndex) {
+      if (
+        Array.prototype.slice.call(select.childNodes).some(function(n) {
+          return n.value === values[selectorIndex];
+        })
+      ) {
+        select.value = values[selectorIndex];
+      }
+    });
+  }
+}
+
+navigator.mediaDevices
+  .enumerateDevices()
+  .then(gotDevices)
+  .catch(handleError);
+
+function gotStream(stream) {
+  arToolkitSource.domElement.srcObject = stream; // make stream available to console
+  // video.srcObject = stream;
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function start() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+  var videoSource = videoSelect.value;
+  var constraints = {
+    video: {
+      deviceId: videoSource ? { exact: videoSource } : undefined
+    }
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(gotStream)
+    .then(gotDevices)
+    .catch(handleError);
+}
+
+videoSelect.onchange = start;
+
+function handleError(error) {
+  console.log("navigator.getUserMedia error: ", error);
+}
+
+start();
+
 // init renderer
 var renderer	= new THREE.WebGLRenderer({
   // antialias	: true,
